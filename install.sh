5dc79e41bec10020122fc438ade701724f4510b1
chungmv87 <chungmv87@gmail.com>
2025-07-13 12:13:24 +0700

    Deploy Java app to CodePipeline

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd /home/ec2-user/myapp
+npm install
+chmod +x scripts/start.sh
+./scripts/start.sh
---
+++ start.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Dừng app cũ nếu đang chạy
+pkill node || true
+
+# Start app ở background
+nohup node index.js > app.log 2>&1 &
