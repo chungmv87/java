245408b9194a47689cb976151a7b2d4671d6d8b7
chungmv87 <chungmv87@gmail.com>
2025-07-13 16:10:37 +0700

    Deploy Java app to CodePipeline

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd /data/myNodejsApp
 chmod +x scripts/start.sh
-cd /data/
-chown ec2-user:ec2-user myNodejsApp -R
 npm install
+cd /data/
+chown -R ec2-user:ec2-user myNodejsApp/
+chmod -R 755 myNodejsApp/
